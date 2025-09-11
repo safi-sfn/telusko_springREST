b117fd64a60861c932195d5974edfa0092b9fa0d
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-11 18:28:39 +0530

    initial commit

---
+++ 02_Actuator/src/test/java/in/inxod/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
