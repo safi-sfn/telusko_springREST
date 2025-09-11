85e0c2c912b1e4ba681dfa580e0e38d47d0f8c88
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-11 11:35:38 +0530

    feat(model) : POJO for Student

---
+++ 01_RestApplication/src/main/java/in/inxod/greeting/model/Student.java
@@ -0,0 +1,50 @@
+package in.inxod.greeting.model;
+
+public class Student {
+
+	private Integer id;
+	private String name;
+	private String city;
+	private Integer age;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Student(Integer id, String name, String city, Integer age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", age=" + age + "]";
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
