4347c13dc03e9d0cf71df1e35c11a23ad2befd3b
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-12 21:25:17 +0530

    feat(controller) : student controller

---
+++ 03_SpringBootProfiles/src/main/java/in/inxod/profiles/controller/StudentController.java
@@ -0,0 +1,27 @@
+package in.inxod.profiles.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.inxod.profiles.model.Student;
+
+@RestController
+public class StudentController {
+
+	@GetMapping("/get-info")
+	public ResponseEntity<Student> getStudentInfo(){
+		Student stu = new Student(1,"Rahul","Haryana",32);
+		return new ResponseEntity<Student>(stu, HttpStatus.OK);
+	}
+	
+	@PostMapping("/add-student")
+	public ResponseEntity<String> addStudent(@RequestBody Student stu){
+		System.out.println(stu);
+		String res = "Student Added successfully";
+		return new ResponseEntity<String>(res,HttpStatus.CREATED);
+	}
+}
