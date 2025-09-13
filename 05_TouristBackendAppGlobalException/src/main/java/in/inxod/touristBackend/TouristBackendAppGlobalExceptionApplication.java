b9f0d4a3333dcc41eaf82130d97ba7cd0c52f33d
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-13 21:39:32 +0530

    initial commit

---
+++ 05_TouristBackendAppGlobalException/src/main/java/in/inxod/touristBackend/TouristBackendAppGlobalExceptionApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.touristBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristBackendAppGlobalExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristBackendAppGlobalExceptionApplication.class, args);
+	}
+
+}
---
+++ 05_TouristBackendAppGlobalException/src/test/java/in/inxod/touristBackend/TouristBackendAppGlobalExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.touristBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristBackendAppGlobalExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
