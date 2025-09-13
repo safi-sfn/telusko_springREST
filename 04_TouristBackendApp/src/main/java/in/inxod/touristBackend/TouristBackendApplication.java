28ee9a39d3cb8f7d232ec20c063b7c9e7772c324
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-13 17:00:44 +0530

    feat(main) : main application to run

---
+++ 04_TouristBackendApp/src/main/java/in/inxod/touristBackend/TouristBackendApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.touristBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristBackendApplication.class, args);
+	}
+
+}
