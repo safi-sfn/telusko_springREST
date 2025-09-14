a67efaff6db8c4e017daf836aec1db0a9350ce43
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-14 07:58:07 +0530

    feat(exception) : tourist not found exception

---
+++ 05_TouristBackendAppGlobalException/src/main/java/in/inxod/touristBackend/exception/TouristNotFoundException.java
@@ -0,0 +1,8 @@
+package in.inxod.touristBackend.exception;
+
+public class TouristNotFoundException extends RuntimeException {
+
+	public TouristNotFoundException(String msg) {
+		super(msg);
+	}
+}
