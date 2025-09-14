2fda7878d7ca91aca54af666bfe03c07f0657a2e
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-14 15:19:13 +0530

    initial commit

---
+++ 07_HateoasApplication/src/main/java/in/inxod/hateoas/HateoasApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasApplication.class, args);
+	}
+
+}
---
+++ 07_HateoasApplication/src/test/java/in/inxod/hateoas/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
