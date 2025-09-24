41850cff5d14c46eb36c52286f7abbf21766d97b
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-24 20:27:20 +0530

    feat(service) : implementation class of Service interface

---
+++ 08_TicketBookingApplication/src/main/java/in/inxod/ticketBooking/service/TicketServiceImpl.java
@@ -0,0 +1,23 @@
+package in.inxod.ticketBooking.service;
+
+import org.springframework.stereotype.Service;
+
+import in.inxod.ticketBooking.model.Passenger;
+import in.inxod.ticketBooking.model.Ticket;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+	@Override
+	public int registerPassenger(Passenger passenger) {
+		
+		return 0;
+	}
+
+	@Override
+	public Ticket generateTicket(int ticketNumber) {
+		
+		return null;
+	}
+
+}
