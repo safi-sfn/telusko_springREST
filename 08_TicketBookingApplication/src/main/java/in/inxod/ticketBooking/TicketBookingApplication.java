bd1d35555d3f1ac2aab6a8e1ee02e3e7ed565afc
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-24 20:24:10 +0530

    feat(main) : main application to run

---
+++ 08_TicketBookingApplication/src/main/java/in/inxod/ticketBooking/TicketBookingApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.ticketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApplication.class, args);
+	}
+
+}
---
+++ 08_TicketBookingApplication/src/test/java/in/inxod/ticketBooking/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.ticketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
