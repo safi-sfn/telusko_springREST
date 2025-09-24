3997ee4464a9d7e861fe073c87c8d4397d30ce7d
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-24 20:26:19 +0530

    feat(service) : service interface class for Ticket

---
+++ 08_TicketBookingApplication/src/main/java/in/inxod/ticketBooking/service/TicketService.java
@@ -0,0 +1,10 @@
+package in.inxod.ticketBooking.service;
+
+import in.inxod.ticketBooking.model.Passenger;
+import in.inxod.ticketBooking.model.Ticket;
+
+public interface TicketService {
+
+	int registerPassenger(Passenger passenger);
+	Ticket generateTicket(int ticketNumber);
+}
