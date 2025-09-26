d00d44e48c5393c34e14bf7627bcd4cae07d8d25
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-26 16:07:31 +0530

    initial commit

---
+++ 09_TicketBookingRestApp/src/test/java/in/inxod/ticketBooking/TicketBookingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.ticketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
