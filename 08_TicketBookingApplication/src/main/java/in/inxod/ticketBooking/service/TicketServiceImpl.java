b63ad5aea2a3f01638453d7f730edc379769c420
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-28 14:16:57 +0530

    feat(service) : webclient logic for different API

--- 08_TicketBookingApplication/src/main/java/in/inxod/ticketBooking/service/TicketServiceImpl.java
+++ 08_TicketBookingApplication/src/main/java/in/inxod/ticketBooking/service/TicketServiceImpl.java
@@ -3,6 +3,7 @@ package in.inxod.ticketBooking.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import in.inxod.ticketBooking.model.Passenger;
 import in.inxod.ticketBooking.model.Ticket;
@@ -16,9 +17,20 @@ public class TicketServiceImpl implements TicketService {
 	@Override
 	public int registerPassenger(Passenger passenger) {
 		// RestTemplate  or // WebClient
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Integer> response = restTemplate.postForEntity(postUrl, passenger, Integer.class);
-		int ticketNumber = response.getBody();
+		
+		// RestTemplate --> it is a class  --> Synchronous in nature
+//		RestTemplate restTemplate = new RestTemplate();
+//		ResponseEntity<Integer> response = restTemplate.postForEntity(postUrl, passenger, Integer.class);
+//		int ticketNumber = response.getBody();
+		
+		// WebClient  --> it is an Interface --> Synchronous and Asynchronous in nature
+		WebClient webClient = WebClient.create();
+		Integer ticketNumber = webClient.post()
+		.uri(postUrl)
+		.bodyValue(passenger)
+		.retrieve()
+		.bodyToMono(Integer.class)
+		.block();
 		return ticketNumber;
 	}
 
@@ -26,9 +38,20 @@ public class TicketServiceImpl implements TicketService {
 	public Ticket generateTicket(int ticketNumber) {
 		
 		// RestTemplate  or // WebClient
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Ticket> response = restTemplate.getForEntity(getUrl, Ticket.class, ticketNumber);
-		Ticket ticket = response.getBody();
+		
+		// RestTemplate
+//		RestTemplate restTemplate = new RestTemplate();
+//		ResponseEntity<Ticket> response = restTemplate.getForEntity(getUrl, Ticket.class, ticketNumber);
+//		Ticket ticket = response.getBody();
+		
+		
+		// WebClient
+		WebClient webClient = WebClient.create();
+		Ticket ticket = webClient.get()
+		.uri(getUrl, ticketNumber)
+		.retrieve()
+		.bodyToMono(Ticket.class)
+		.block();
 		
 		return ticket;
 	}
