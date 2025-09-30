e5e607938e8756935522b5e57d607b3fd9179a94
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-30 22:07:12 +0530

    initial commit

---
+++ 12_KafkaPublisher/src/test/java/in/inxod/kafka/KafkaPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
