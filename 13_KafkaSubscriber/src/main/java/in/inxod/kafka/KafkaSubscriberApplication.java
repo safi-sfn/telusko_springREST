3390860433bca519ea10f41bd322ed48b0c4139c
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-03 09:22:50 +0530

    feat(main) : print msg from kafka server to in subscriber

--- 13_KafkaSubscriber/src/main/java/in/inxod/kafka/KafkaSubscriberApplication.java
+++ 13_KafkaSubscriber/src/main/java/in/inxod/kafka/KafkaSubscriberApplication.java
@@ -2,12 +2,21 @@ package in.inxod.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import in.inxod.kafka.util.AppConstant;
 
 @SpringBootApplication
 public class KafkaSubscriberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSubscriberApplication.class, args);
+	}
+	
+	@KafkaListener(topics=AppConstant.TOPIC_NAME, groupId="group_inxod_customer")
+	public void subscribeMsgFromKafkaTopic(String cxData) {
+		System.out.println("Message is comming from kafka server");
+		System.out.println(cxData);
 	}
 
 }
