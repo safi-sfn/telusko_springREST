67803cef2562c8d8a0a802482874faed46f1c594
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-03 09:21:20 +0530

    feat(controller) : added endpoint add-cx

--- 12_KafkaPublisher/src/main/java/in/inxod/kafka/controller/CustomerController.java
+++ 12_KafkaPublisher/src/main/java/in/inxod/kafka/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package in.inxod.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import in.inxod.kafka.model.Customer;
 import in.inxod.kafka.service.KafkaService;
 
 @RestController
@@ -10,5 +13,10 @@ public class CustomerController {
 	
 	@Autowired
 	private KafkaService service;
+	
+	@PostMapping("/add-cx")
+	public String addCustomer(@RequestBody Customer cx) {
+		return service.addCxMsg(cx);
+	}
 
 }
